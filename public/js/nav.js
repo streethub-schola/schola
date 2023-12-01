2c2503cb590f4826433aa01310d450b703c49b4f
Godwin <godwinkachi@hotmail.com>
2023-12-01 05:27:31 +0100

    Completed the staff page design

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,12 @@
-const navLinks = document.querySelector('#navbar');
-const navSwitch = document.querySelector('#nav_toggle');
-const mobileView = document.querySelector('#mobile-menu');
+const navLinks = document.querySelector("#navbar");
+const navSwitch = document.querySelector("#nav_toggle");
+const mobileView = document.querySelector("#mobile-menu");
 
+navSwitch.addEventListener("click", () => {
+  mobileView.classList.toggle("flex");
+  mobileView.classList.toggle("hidden");
+});
 
-navSwitch.addEventListener('click', ()=>{
-    mobileView.classList.toggle('flex');
-    mobileView.classList.toggle('hidden')
-
-   
-
-    
-})
-
-mobileView.addEventListener('click', ()=> {
-    mobileView.classList.toggle('hidden')
-})
+mobileView.addEventListener("click", () => {
+  mobileView.classList.toggle("hidden");
+});

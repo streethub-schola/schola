27982930ca6884c91cd9cc9033499a22d847ccba
Godwin <godwinkachi@hotmail.com>
2023-12-25 18:29:16 +0100

    Syncing my branch with portal frontend

--- public/js/login.js
+++ public/js/login.js
@@ -2,16 +2,6 @@
 let form = document.querySelector('form');
 
 
-// function geterror() {
-//     fetch('https://scholabe.myf2.net/api/studentapi/studentlogin.php')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById('errorMsg').innerHTML = data.message;
-//         document.getElementById('errorMsg').style.display = 'block'
-//     })
-// }
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -36,55 +26,3 @@ form.addEventListener('submit', (e) => {
     .catch(err => console.log(err))
     
 })
-
-// function fetchuser() {
-//     fetch('https://scholabe.myf2.net/api/studentapi/getstudents.php')
-//     .then(response => response.json())
-//     .then(data => {
-//         let username = form.username.value;
-//         let password = form.password.value;
-
-
-// })
-
-// function geterror() {
-//     fetch('https://scholabe.myf2.net/api/studentapi/studentlogin.php')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById('errorMsg').innerHTML = data.message;
-//         document.getElementById('errorMsg').style.display = 'block'
-//     })
-// }
-
-// // form.addEventListener('submit', (e) => {
-// //     e.preventDefault();
-
-// //     let username = form.username.value;
-// //     let password = form.password.value;
-// //     fetchuser()
-// // })
-
-// function fetchuser() {
-//     fetch('https://scholabe.myf2.net/api/studentapi/getstudents.php')
-//     .then(response => response.json())
-//     .then(data => {
-//         let username = form.username.value;
-//         let password = form.password.value;
-
-
-//         data.records.forEach(user => {
-//             if (username == user.admin_no && password == user.password) {
-//                 sessionStorage.setItem('loggedInUser', JSON.parse(user));
-//                 // location.href = './admin/index.html'
-//                 console.log(user)
-//             } else {
-//                 // geterror()
-//                 console.log('Data not found in the db')
-//             }
-            
-//         });
-//     })
-
-// }
-

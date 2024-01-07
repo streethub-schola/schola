1c98bf6f8ae342e12a15c8fc982e0b49fa86e881
profinnocent <innocent.unachukwu@gmail.com>
2024-01-07 09:25:01 +0100

    updated login.js to redirect on success

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,33 @@
 // https://scholabe.myf2.net/api/studentapi/studentlogin.php
-let form = document.querySelector('form');
+let form = document.querySelector("form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const userData = {
+    admin_no: `${form.username.value}`,
+    password: `${form.password.value}`,
+  };
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    
-    const userData = {
-        admin_no: `${form.username.value}`,
-        password: `${form.password.value}`
-    };
+  const configData = {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  };
 
-    const configData = {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(userData)
-    };
-   
+  // console.log (userData)
 
-    // console.log (userData)
-
-
-    fetch('https://schola.myf2.net/api/studentapi/studentlogin.php', configData)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-    
-})
+  fetch("https://schola.myf2.net/api/studentapi/studentlogin.php", configData)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.status == 1) {
+        alert(data.message);
+        location.href = "./admin/students/view_student.html";
+      }
+    })
+    .catch((err) => console.log(err));
+});

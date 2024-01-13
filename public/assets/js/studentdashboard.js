56a3b9e07bd16391c7a076d31ede74d30513b54f
Godwin <godwinkachi@hotmail.com>
2024-01-14 00:37:17 +0100

    Minor changes on all resources for the dashboard

--- public/assets/js/studentdashboard.js
+++ public/assets/js/studentdashboard.js
@@ -13,10 +13,9 @@ const registered_at = document.getElementById('registered_at');
 
 const verifier = sessionStorage.getItem('schola-user');
 
-if (verifier) {
-   console.log('user is logged in')
-} else {
-    location.href = '../students/login.html';
+if (!verifier) {
+        location.href = '../students/login.html';
+   //console.log('user is logged in')
 }
 
 // Get the data from the storage
@@ -32,3 +31,8 @@ pgrel.innerHTML = `${data.student.guardian_rel}`;
 // const crdate = data.student.created_at
 // const padate = crdate.split(" ")[0]
 registered_at.innerHTML = `${data.student.created_at}`;
+
+
+
+
+
--- public/assets/js/studentlogout.js
+++ public/assets/js/studentlogout.js
@@ -1,3 +1,42 @@
+const logoutBtn = document.getElementById('logout');
 //CHECK IF A SESSION IS EXISTING
 
-const verifier = sessionStorage.getItem('schola-user')
+const vericheck = sessionStorage.getItem('schola-user');
+
+//console.log(vericheck)
+
+const reusable_data = JSON.parse(vericheck)
+//console.log(reusable_data)
+
+const userdata = {admin_no: reusable_data.student.admin_no}
+
+console.log(userdata)
+
+
+const configData = {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: userdata
+  };
+
+
+logoutBtn.addEventListener('click', ()=> {
+    
+if (verifier){
+    fetch('https://schola.skaetch.com/api/studentapi/studentlogout.php', configData)
+    .then(res => res.json())
+    .then(data => {
+        if (data.status == 'ok'){
+            sessionStorage.removeItem('schola-user');
+            location.href = '../students/login.html'
+        }else {
+         console.log(data.message)
+       }
+    })
+    .catch(err => console.log(err))
+    
+}
+})

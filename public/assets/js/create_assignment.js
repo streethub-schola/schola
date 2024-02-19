b97c71acf305b3eac2b9a5d7df3525321c4ee74a
MiracleChibuike <miraclechibuike27@gmail.com>
2024-02-19 16:49:20 +0100

    update create assignment page

--- public/assets/js/create_assignment.js
+++ public/assets/js/create_assignment.js
@@ -5,7 +5,7 @@ var textMessage = document.getElementById("assignmentText");
 var remove_Question = document.getElementById("remove");
 var btn_Submit = document.getElementById("submitBtn");
 var sname = document.getElementById("staff-name")
-
+.ATTRIBUTE_NODE.
 
 add_Question.addEventListener("click", ()=>{
     

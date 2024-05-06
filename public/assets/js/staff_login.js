72f25fa1b4fb50c1c8c00d3cce19fe210ba1cd5a
Godwin <godwinkachi@hotmail.com>
2024-05-06 16:00:58 +0100

    pulled update

--- public/assets/js/staff_login.js
+++ public/assets/js/staff_login.js
@@ -1,52 +1,45 @@
-portal_frontend
 let form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-    const userData = {
-      staff_no: `${form.staff_no.value}`,
-      password: `${form.password.value}`,
-    }
-    
-    console.log(userData);
+  const userData = {
+    staff_no: `${form.staff_no.value}`,
+    password: `${form.password.value}`,
+  };
 
- const configData = {
+  console.log(userData);
+
+  const configData = {
     method: "POST",
     mode: "no-cors",
     headers: {
       "Content-Type": "application/json",
-      "Accept": "application/json"
+      Accept: "application/json",
     },
-    body: JSON.stringify(userData)
+    body: JSON.stringify(userData),
   };
 
-
-  logteacher(configData)
-
+  logteacher(configData);
 });
 
+function logteacher(teacherObject) {
+  fetch("https://schola-2.myf2.net/api/staffapi/stafflogin.php", teacherObject)
+    // fetch("http://localhost/schola-2/api/staffapi/stafflogin.php", teacherObject)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.status == 1) {
+        alert(data.message);
 
+        // Save logged in user info to session storage so you can access them in other pages
+        sessionStorage.setItem("scola-staff", JSON.stringify(data));
 
-function logteacher(teacherObject){
+        location.href = "../../teachers/teacher_dashboard.html";
+      } else {
+        alert(data.message);
+      }
+    })
 
-
-      fetch("http://localhost/schola-2/api/staffapi/stafflogin.php", configData)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.status == 1) {
-          alert(data.message);
-          
-          // Save logged in user info to session storage so you can access them in other pages
-          sessionStorage.setItem("scola-staff", JSON.stringify(data) )
-          
-          location.href = "../../teachers/teacher_dashboard.html";
-        }
-        else {
-          alert(data.message);
-        }
-      })
-      
-      .catch((err) => console.log("Error is on", err));
-  });
+    .catch((err) => console.log("Error is on", err));
+}

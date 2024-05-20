9e4d5aa8194a085eb3e8667ea3f699408128a3f9
Godwin <godwinkachi@hotmail.com>
2024-05-20 16:09:56 +0100

    Merge conflict resolved

--- public/assets/js/staff_login.js
+++ public/assets/js/staff_login.js
@@ -4,48 +4,42 @@ let form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const userData = {
-    staff_no: `${form.staff_no.value}`,
-    password: `${form.password.value}`,
-  };
+    const userData = {
+      staff_no: `${form.staff_no.value}`,
+      password: `${form.password.value}`,
+    }
+    
+    console.log(userData);
 
-  console.log(userData);
-
-  const configData = {
+ const configData = {
     method: "POST",
     mode: "no-cors",
     headers: {
       "Content-Type": "application/json",
-      Accept: "application/json",
+      "Accept": "application/json"
     },
-    body: JSON.stringify(userData),
+    body: JSON.stringify(userData)
   };
 
-  logteacher(configData)
+
+  fetch(API_DOMAIN+"/api/staffapi/stafflogin.php", configData)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    if (data.status == 1) {
+      alert(data.message);
+      
+      // Save logged in user info to session storage so you can access them in other pages
+      sessionStorage.setItem("schola-staff", JSON.stringify(data) )
+      
+      location.href = "../../public/teachers/teacher_dashboard.html";
+    }
+    else {
+      alert(data.message);
+    }
+  })
+  .catch((err) => console.log("Error is on", err));
 
 });
 
 
-
-function logteacher(teacherObject){
-
-
-      fetch("http://localhost/schola-2/api/staffapi/stafflogin.php", configData)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        if (data.status == 1) {
-          alert(data.message);
-          
-          // Save logged in user info to session storage so you can access them in other pages
-          sessionStorage.setItem("scola-staff", JSON.stringify(data) )
-          
-          location.href = "../../teachers/teacher_dashboard.html";
-        }
-        else {
-          alert(data.message);
-        }
-      })
-      
-      .catch((err) => console.log("Error is on", err));
-  };

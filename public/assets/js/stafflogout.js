7d71dfd0a4e4deda699cbda8ed0abe6423624e02
Godwin <godwinkachi@hotmail.com>
2024-06-01 20:32:22 +0100

    Changed the file names for student resources (JS)

--- public/assets/js/stafflogout.js
+++ public/assets/js/stafflogout.js
@@ -1,18 +1,16 @@
 const logoutBtn = document.getElementById('logout');
 const logoutMobBtn = document.getElementById('logoutmob');
 
-//CHECK IF A SESSION IS EXISTING
-
 const vericheck = sessionStorage.getItem('schola-user');
 
 //console.log(vericheck)
 
 const reusable_data = JSON.parse(vericheck)
-//console.log(reusable_data)
+console.log(reusable_data)
 
-const userdata = {staff_no: reusable_data.staff_no}
+//CHECK IF A SESSION IS EXISTING
 
-// console.log(userdata)
+const userdata = {staff_no: reusable_data.staff_no};
 
 
 const configData = {
@@ -32,13 +30,25 @@ logoutMobBtn.addEventListener('click', logout)
 
 
 function logout(){
+  
+if(!reusable_data){
+  alert('You do not have permission to view this page.');
+    location.href = '../teachers/teacher_login.html';
+}
+
+
+
+// console.log(userdata)
+
+
   if (verifier){
-    fetch('https://schola.skaetch.com/api/studentapi/studentlogout.php', configData)
+    fetch('https://schola.skaetch.com/api/studentapi/stafflogout.php', configData)
     .then(res => res.json())
     .then(data => {
         if (data.status == 1){
             sessionStorage.removeItem('schola-user');
-            location.href = '../students/login.html'
+            alert('Sucessfully Logged out.');
+            location.href  = '../teachers/teacher_login.html';
         }
     })
     .catch(err => alert("There is an issue with your network. Please try again"))
